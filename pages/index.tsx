39ea904c564a2efc47b7fe9d1593ee11cac46d79
trevorwright <trevor.wright@prodigygame.com>
2021-11-22 20:47:04 -0500

    Adds og meta fields

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Hero from '../components/Hero';
 import AboutMe from '../components/AboutMe';
 import Skills from '../components/Skills';
@@ -7,6 +8,15 @@ import Footer from '../components/Footer';
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Trevor Wright</title>
+        <meta property="og:type" content="profile"></meta>
+        <meta property="og:title" content="Leader and Web Developer"></meta>
+        <meta property="og:url" content="https://trevorwright.ca"></meta>
+        <meta property="og:image" content="/images/profile.png"></meta>
+        <meta property="profile:first_name" content="Trevor"></meta>
+        <meta property="profile:last_name" content="Wright"></meta>
+      </Head>
       <Hero></Hero>
       <AboutMe></AboutMe>
       <Skills></Skills>
